f0947a69f32e2f17b656ca65ae1816c7d89ee541
Anas <135501348+anasfanani@users.noreply.github.com>
2025-04-22 16:15:29 +0000

    fix: android ssh groupids

--- util/osuser/group_ids.go
+++ util/osuser/group_ids.go
@@ -19,6 +19,9 @@ import (
 // an error. It will first try to use the 'id' command to get the group IDs,
 // and if that fails, it will fall back to the user.GroupIds method.
 func GetGroupIds(user *user.User) ([]string, error) {
+	if runtime.GOOS == "android" {
+		return []string{"0"}, nil
+	}
 	if runtime.GOOS != "linux" {
 		return user.GroupIds()
 	}
